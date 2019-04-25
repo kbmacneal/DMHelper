4654a3ba1d12bf17b946a04f519b805c32875538
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-04-25 15:59:56 -0500

    starting screen work

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DM_helper;
+using DM_helper.Archetypes;
+using DM_helper.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using DM_helper;
-using DM_helper.Models;
 
 namespace DM_helper.Controllers
 {
@@ -14,44 +15,45 @@ namespace DM_helper.Controllers
     {
         private readonly Context _context;
 
-        public CharacterController(Context context)
+        public CharacterController (Context context)
         {
             _context = context;
         }
 
         // GET: Character
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index ()
         {
-            var context = _context.Character.Include(c => c.Background).Include(c => c.Gender);
-            return View(await context.ToListAsync());
+            var context = _context.Character;
+            return View (await context.ToListAsync ());
         }
 
         // GET: Character/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details (int? id)
         {
             if (id == null)
             {
-                return NotFound();
+                return NotFound ();
             }
 
             var character = await _context.Character
-                .Include(c => c.Background)
-                .Include(c => c.Gender)
-                .FirstOrDefaultAsync(m => m.ID == id);
+                .Include (c => c.Background)
+                .Include (c => c.Gender)
+                .FirstOrDefaultAsync (m => m.ID == id);
             if (character == null)
             {
-                return NotFound();
+                return NotFound ();
             }
 
-            return View(character);
+            return View (character);
         }
 
         // GET: Character/Create
-        public IActionResult Create()
+        public IActionResult Create ()
         {
-            ViewData["BackgroundID"] = new SelectList(_context.Set<Background>(), "ID", "ID");
-            ViewData["GenderID"] = new SelectList(_context.Set<Gender>(), "ID", "ID");
-            return View();
+            ViewData["BackgroundID"] = new SelectList (_context.Set<BackgroundArchetype> (), "ID", "Name");
+            ViewData["GenderID"] = new SelectList (_context.Set<GenderArchetype> (), "ID", "Name");
+            ViewData["ArmorID"] = new SelectList (_context.Set<ArmorArchetype> (), "ID", "Name");
+            return View ();
         }
 
         // POST: Character/Create
@@ -59,35 +61,55 @@ namespace DM_helper.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,BackgroundID,GenderID,Faction,Homeworld,CurrentHP,MaxHP,CurrentSystemStrain,MaxSystemStrain,PermanentStrain,CurrentXP,XPTilNextLevel,AC,AtkBonus,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Credits,ArmorID,Goals,Notes")] Character character)
+        public async Task<IActionResult> Create ([Bind ("ID,Name,BackgroundID,GenderID,Faction,Homeworld,CurrentHP,MaxHP,CurrentSystemStrain,MaxSystemStrain,PermanentStrain,CurrentXP,XPTilNextLevel,AC,AtkBonus,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Credits,ArmorID,Goals,Notes")] Character character)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(character);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                Background bg = new Background (await _context.BackgroundArchetype.FirstOrDefaultAsync (e => e.ID == character.BackgroundID));
+
+                bg.CharacterID=character.ID;
+
+                Gender gen = new Gender (await _context.GenderArchetype.FirstOrDefaultAsync (e => e.ID == character.GenderID));
+
+                gen.CharacterID=character.ID;
+
+                Armor arm = new Armor (await _context.ArmorArchetype.FirstOrDefaultAsync (e => e.ID == character.ArmorID));
+
+                arm.CharacterID=character.ID;
+
+                character.Background = bg;
+                character.Gender = gen;
+                character.Armor = arm;
+
+                _context.Add (character);
+                _context.Add (bg);
+                _context.Add (gen);
+                _context.Add (arm);
+
+                await _context.SaveChangesAsync ();
+                return RedirectToAction (nameof (Index));
             }
-            ViewData["BackgroundID"] = new SelectList(_context.Set<Background>(), "ID", "ID", character.BackgroundID);
-            ViewData["GenderID"] = new SelectList(_context.Set<Gender>(), "ID", "ID", character.GenderID);
-            return View(character);
+            ViewData["BackgroundID"] = new SelectList (_context.Set<BackgroundArchetype> (), "ID", "Name", character.BackgroundID);
+            ViewData["GenderID"] = new SelectList (_context.Set<GenderArchetype> (), "ID", "Name", character.GenderID);
+            return View (character);
         }
 
         // GET: Character/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit (int? id)
         {
             if (id == null)
             {
-                return NotFound();
+                return NotFound ();
             }
 
-            var character = await _context.Character.FindAsync(id);
+            var character = await _context.Character.FindAsync (id);
             if (character == null)
             {
-                return NotFound();
+                return NotFound ();
             }
-            ViewData["BackgroundID"] = new SelectList(_context.Set<Background>(), "ID", "ID", character.BackgroundID);
-            ViewData["GenderID"] = new SelectList(_context.Set<Gender>(), "ID", "ID", character.GenderID);
-            return View(character);
+            ViewData["BackgroundID"] = new SelectList (_context.Set<BackgroundArchetype> (), "ID", "Name", character.BackgroundID);
+            ViewData["GenderID"] = new SelectList (_context.Set<GenderArchetype> (), "ID", "Name", character.GenderID);
+            return View (character);
         }
 
         // POST: Character/Edit/5
@@ -95,72 +117,72 @@ namespace DM_helper.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,BackgroundID,GenderID,Faction,Homeworld,CurrentHP,MaxHP,CurrentSystemStrain,MaxSystemStrain,PermanentStrain,CurrentXP,XPTilNextLevel,AC,AtkBonus,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Credits,ArmorID,Goals,Notes")] Character character)
+        public async Task<IActionResult> Edit (int id, [Bind ("ID,Name,BackgroundID,GenderID,Faction,Homeworld,CurrentHP,MaxHP,CurrentSystemStrain,MaxSystemStrain,PermanentStrain,CurrentXP,XPTilNextLevel,AC,AtkBonus,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Credits,ArmorID,Goals,Notes")] Character character)
         {
             if (id != character.ID)
             {
-                return NotFound();
+                return NotFound ();
             }
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(character);
-                    await _context.SaveChangesAsync();
+                    _context.Update (character);
+                    await _context.SaveChangesAsync ();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!CharacterExists(character.ID))
+                    if (!CharacterExists (character.ID))
                     {
-                        return NotFound();
+                        return NotFound ();
                     }
                     else
                     {
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction (nameof (Index));
             }
-            ViewData["BackgroundID"] = new SelectList(_context.Set<Background>(), "ID", "ID", character.BackgroundID);
-            ViewData["GenderID"] = new SelectList(_context.Set<Gender>(), "ID", "ID", character.GenderID);
-            return View(character);
+            ViewData["BackgroundID"] = new SelectList (_context.Set<BackgroundArchetype> (), "ID", "Name", character.BackgroundID);
+            ViewData["GenderID"] = new SelectList (_context.Set<GenderArchetype> (), "ID", "Name", character.GenderID);
+            return View (character);
         }
 
         // GET: Character/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete (int? id)
         {
             if (id == null)
             {
-                return NotFound();
+                return NotFound ();
             }
 
             var character = await _context.Character
-                .Include(c => c.Background)
-                .Include(c => c.Gender)
-                .FirstOrDefaultAsync(m => m.ID == id);
+                .Include (c => c.Background)
+                .Include (c => c.Gender)
+                .FirstOrDefaultAsync (m => m.ID == id);
             if (character == null)
             {
-                return NotFound();
+                return NotFound ();
             }
 
-            return View(character);
+            return View (character);
         }
 
         // POST: Character/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost, ActionName ("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed (int id)
         {
-            var character = await _context.Character.FindAsync(id);
-            _context.Character.Remove(character);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            var character = await _context.Character.FindAsync (id);
+            _context.Character.Remove (character);
+            await _context.SaveChangesAsync ();
+            return RedirectToAction (nameof (Index));
         }
 
-        private bool CharacterExists(int id)
+        private bool CharacterExists (int id)
         {
-            return _context.Character.Any(e => e.ID == id);
+            return _context.Character.Any (e => e.ID == id);
         }
     }
 }
