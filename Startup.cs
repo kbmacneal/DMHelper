c066d93b67665e5057089ea15e66009a503a5585
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-07-06 16:52:14 -0500

    removing cookie consent, its not needed

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace DM_helper
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
